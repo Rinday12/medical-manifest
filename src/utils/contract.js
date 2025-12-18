b5daf24a14879c5cdda91fa710380ab56b467a5c
Rinday12 <rindayzildzianisalji@gmail.com>
2025-12-18 09:08:38 +0700

    refactor Manifest

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -4,16 +4,26 @@ import MedicalCID from "../contracts/MedicalCID.json";
 const CONTRACT_ADDRESS = "0x87553d051e30d35cfcd80bc6ee67f84ad9d42864";
 
 export const sendCIDToSmartContract = async (cid) => {
-  if (!window.ethereum) throw new Error("MetaMask is not installed");
+  if (!window.ethereum) throw new Error("MetaMask not installed");
 
   await window.ethereum.request({ method: "eth_requestAccounts" });
 
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
 
-  // MedicalCID adalah array ABI, langsung dipakai
-  const contract = new ethers.Contract(CONTRACT_ADDRESS, MedicalCID, signer);
+  const contract = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    MedicalCID,
+    signer
+  );
 
-  const tx = await contract.storeCID(cid);
+  // 🔥 LEGACY GAS (ANTI METAMASK BUG)
+  const tx = await contract.storeCID(cid, {
+    gasLimit: 300000n,
+    gasPrice: ethers.parseUnits("10", "gwei"), // 👈 PENTING
+  });
+
+  console.log("Tx sent:", tx.hash);
   await tx.wait();
+  console.log("Tx confirmed");
 };
